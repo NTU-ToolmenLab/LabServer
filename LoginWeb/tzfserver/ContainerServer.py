d98e1c15aee92b53035b8a3159af7b4a354238a7
linnil1 <linnil1.886@gmail.com>
2018-02-23 21:36:52 +0800

    Use docker in server

--- LoginWeb/tzfserver/ContainerServer.py
+++ LoginWeb/tzfserver/ContainerServer.py
@@ -1,36 +1,43 @@
 from .start import set_db, query_db
 import time
+import docker
 class UserError:
     pass
 
-import time
 class User:
     def __init__(self, name):
         self.name = name
+        self.con = docker.from_env().containers
+        self.netName = "labserver_mynet"
 
-    def getToken(self, queryStr, queryObj):
-        qdata = query_db(queryStr, queryObj, one=True)
-        if not qdata:
-            raise UserError
+    def getToken(self, queryStr, queryObj, one=True):
+        qdata = query_db(queryStr, queryObj, one=one)
         return qdata
 
     def lists(self):
         print("list", self.name)
-        ddata = self.getToken("SELECT * FROM tokens WHERE name = ?", (self.name,))
-        return [{"id"  : ddata['boxid'],
-                 "name": ddata['boxname'],
-                 "time": ddata['time'] }]
+        ddata = self.getToken("SELECT * FROM tokens WHERE name = ?", (self.name,), one=False)
+        return [{"id"  : d['boxid'],
+                 "name": d['boxname'],
+                 "time": d['time'] } for d in ddata]
 
     def reset(self, containerID):
         print("reset", containerID)
-        return packed[0]
         return True
 
     def resume(self, containerID):
         ddata = self.getToken("SELECT * FROM tokens WHERE name = ? AND boxid = ?",
                               (self.name, containerID))
-        set_db("UPDATE tokens SET time = ? WHERE boxid = ?", (str(time.time()), containerID))
+        if not ddata:
+            raise UserError
         mytoken = ddata["tokenname"]
+
+        container = self.con.get(containerID)
+        set_db("UPDATE tokens SET time = ?, tokenip = ? WHERE boxid = ?",
+               (str(time.time()),
+                container.attrs['NetworkSettings']['Networks'][self.netName]['IPAddress'],
+                containerID))
+        # my_vnc code should add 5900 by itself
         print("resume", dict(ddata))
         return mytoken
 
