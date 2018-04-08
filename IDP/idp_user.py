16e1d1b3781f91ef54d403208afd4b733c5c1d05
linnil1 <linnil1.886@gmail.com>
2018-04-08 23:53:26 +0800

    Add idp server example by pysaml2

---
+++ IDP/idp_user.py
@@ -0,0 +1,48 @@
+# from dirg_util.dict import LDAPDict
+# ldap_settings = {
+#    "ldapuri": "ldaps://ldap.test.umu.se",
+#    "base": "dc=umu, dc=se",
+#    "filter_pattern": "(uid=%s)",
+#    "user": "",
+#    "passwd": "",
+#    "attr": [
+#        "eduPersonScopedAffiliation",
+#        "eduPersonAffiliation",
+#        "eduPersonPrincipalName",
+#        "givenName",
+#        "sn",
+#        "mail",
+#        "uid",
+#        "o",
+#        "c",
+#        "labeledURI",
+#        "ou",
+#        "displayName",
+#        "norEduPersonLIN"
+#    ],
+#    "keymap": {
+#        "mail": "email",
+#        "labeledURI": "labeledURL",
+#    },
+#    "static_values": {
+#        "eduPersonTargetedID": "one!for!all",
+#    },
+#    "exact_match": True,
+#    "firstonly_len1": True,
+#    "timeout": 15,
+# }
+# Uncomment to use a LDAP directory instead.
+# USERS = LDAPDict(**ldap_settings)
+
+USERS = {
+    "testuser": {
+        "uid": "testuser",
+        'quota': "1GB",
+        "mail": "123@123"
+    }
+}
+
+PASSWD = {
+    "testuser": "test",
+}
+
