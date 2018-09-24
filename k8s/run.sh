7e6864abf923b25d312100765a74e1c24abe2370
linnil1 <linnil1.886@gmail.com>
2018-09-24 18:43:56 +0800

    Convert all docker-compose to k8s

--- k8s/run.sh
+++ k8s/run.sh
@@ -13,4 +13,4 @@ sed -i "s/my.domain.ntu.edu.tw/$domain_name/g" ../Nextcloud/nginx*
 sed -i "s/my.domain.ntu.edu.tw/$domain_name/g" nextcloud_collabora.yml
 sed -i "s/mysql_root_password/$mysql_root_password/g" nextcloud_db.yml
 sed -i "s/mysql_passowrd/$mysql_password/g" nextcloud_db.yml
-sed -i "s/\~/$home/g" nextcloud_*.yml
+sed -i "s/\~/$home/g" *.yml
