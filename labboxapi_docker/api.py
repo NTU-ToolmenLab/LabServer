c47edd684b49b86db8cb23a5abc2d540b57bd0cc
linnil1 <linnil1.886@gmail.com>
2020-01-15 03:42:36 +0800

    Refactor: Add docstring on labboxapi-docker

--- labboxapi_docker/api.py
+++ labboxapi_docker/api.py
@@ -5,57 +5,95 @@ import os
 
 
 app = Flask(__name__)
-app.secret_key = 'super secret string'  # Change this!
-label = 'UserDocker'
-default_homepath = '/nashome/'
-default_naspath = '/home/nas/'
+app.secret_key = "super secret string"  # Change this!
+label = "UserDocker"
 client = docker.from_env()
+debug = True
 
 
-@app.errorhandler(403)
-def Error(e):
+@app.errorhandler(Exception)
+def AllError(error):
+    """
+    Catch all error here.
+
+    This function will call if the error cannot handle by my code.
+
+    Returns
+    -------
+    json
+        The status code will be 500.
+        There will have a key named "status" and its value is 500.
+        The message will show why this happened.
+    """
     message = {
-        'status': 403,
-        'message': str(e) or 'Internal Error'
+        'status': 500,
+        'message': "Internal Error: " + str(error)
     }
+    if debug:
+        print(error)
     resp = jsonify(message)
-    resp.status_code = 403
+    resp.status_code = 500
     return resp
 
 
-@app.errorhandler(404)
-def not_found(error=None):
+@app.errorhandler(400)
+def Error(error):
+    """
+    Handle error here.
+
+    Returns
+    -------
+    json
+        The status code will be 400.
+        There will have a key named "status" and its value is 400.
+        The message will show why this happened.
+    """
+    if debug:
+        print(error)
     message = {
-        'status': 404,
-        'message': str(error) or 'Not Found'
+        'status': 400,
+        'message': str(error)
     }
     resp = jsonify(message)
-    resp.status_code = 404
+    resp.status_code = 400
     return resp
 
 
-def Ok():
+def Ok(data={}):
+    """
+    Handle all return data here.
+
+    Returns
+    -------
+    json
+        The status code will be 200.
+        There will have a "status" key and its value is 200.
+        All the return data will in "data" key.
+    """
     return jsonify({
         'status': 200,
-        'message': 'ok'
+        'message': "OK",
+        'data': data
     })
 
 
-def getContainer(query, one=True):
+def getContainer(id):
+    """Ouery the container by id"""
     try:
-        container = client.containers.get(query)
-        '''
+        container = client.containers.get(id)
+        """
+        # TODO
         # for k8s label is in another containers
+        """
         if label not in container.labels:
-            abort(404)
-        '''
+            abort(400, "Not in the same namespace")
         return container
     except docker.errors.NotFound:
-        abort(404, 'Not Found Container')
-        # raise Error('Not Found Container')
+        abort(400, "Container Not Found")
 
 
 def parseContainer(cont):
+    """Return a json object from container object"""
     return {
         'name': cont.name,
         'image': cont.attrs['Config']['Image'],
@@ -67,9 +105,17 @@ def parseContainer(cont):
     }
 
 
-@app.route('/search', methods=['POST'])
+@app.route("/search", methods=["POST"])
 def search():
-    query = request.form.get('name')
+    """
+    Search the container by ID.
+
+    Parameters
+    ----------
+    name: str
+        The container ID. Empty will show all containers
+    """
+    query = request.form.get("name")
     output = []
     if not query:
         allcontainer = client.containers.list(filters={'label': label})
@@ -78,159 +124,241 @@ def search():
         container = getContainer(query)
         output = parseContainer(container)
 
-    return jsonify(output)
+    return Ok(output)
 
 
-@app.route('/searchimage', methods=['POST'])
+@app.route("/search/image", methods=["POST"])
 def searchImage():
-    query = request.form.get('name')
-    output = []
+    """
+    Search the images by ID.
+
+    Parameters
+    ----------
+    name: str
+        The tag name of image.
+    """
+    query = request.form.get("name")
 
     try:
         img = client.images.get(query)
     except docker.errors.ImageNotFound:
-        return not_found('Not found Image')
+        abort(400, "Image Not Found")
 
-    output = {'tag': img.tags[0],
-              'date': img.attrs['Metadata']['LastTagTime']}
+    return Ok({
+            'tag': img.tags[0],
+            'date': img.attrs['Metadata']['LastTagTime']})
 
-    return jsonify(output)
 
-
-@app.route('/start', methods=['POST'])
+@app.route("/start", methods=["POST"])
 def start():
-    name = request.form.get('name')
+    """Start the container by name=ID"""
+    name = request.form.get("name")
     container = getContainer(name)
     container.start()
     return Ok()
 
 
-@app.route('/stop', methods=['POST'])
+@app.route("/stop", methods=["POST"])
 def stop():
-    name = request.form.get('name')
+    """Stop the container by name=ID"""
+    name = request.form.get("name")
     container = getContainer(name)
     container.stop()
     return Ok()
 
 
-@app.route('/restart', methods=['POST'])
+@app.route("/restart", methods=["POST"])
 def restart():
-    name = request.form.get('name')
+    """Restart the container by name=ID"""
+    name = request.form.get("name")
     container = getContainer(name)
     container.restart()
     return Ok()
 
 
-@app.route('/passwd', methods=['POST'])
+@app.route("/passwd", methods=["POST"])
 def passwd():
-    name = request.form.get('name')
+    """
+    Change the password of user(ubuntu).
+
+    Parameters
+    ----------
+    name: str
+        The container ID.
+    pw: str
+        The sha512 encrypted password
+    """
+    name = request.form.get("name")
     container = getContainer(name)
     if not container.attrs['State']['Running']:
-        abort(404)
-    pw = request.form.get('pw')
+        abort(400, "Not Running")
+    pw = request.form.get("pw")
     rest = container.exec_run('usermod -p "' + pw + '" ubuntu')
     return Ok()
 
 
-@app.route('/commit', methods=['POST'])
+@app.route("/commit", methods=["POST"])
 def commit():
-    name = request.form.get('name')
-    newname = request.form.get('newname', name)
+    """
+    Commit the container to image.
+
+    Parameters
+    ----------
+    name: str
+        The container ID.
+    newname: str
+        The tag name.
+    """
+    name = request.form.get("name")
+    newname = request.form.get("newname", name)
     container = getContainer(name)
     container.commit(newname)
     return Ok()
 
 
-@app.route('/push', methods=['POST'])
+@app.route("/push", methods=["POST"])
 def push():
-    name = request.form.get('name')
+    """
+    Push the image to somewhere(Harbor).
+
+    Parameters
+    ----------
+    name: str
+        The container ID.
+    username: str
+        The user name of somewhere
+    password: str
+        The password of the user
+    registry: url
+        The url of somewhere
+    """
+    # check image
+    name = request.form.get("name")
     try:
         client.images.get(name)
     except docker.errors.ImageNotFound:
-        return not_found('Not Find Image')
+        abort(400, "Not Find Image")
 
-    username = request.form.get('username')
-    password = request.form.get('password')
-    registry = request.form.get('registry')
+    # check registry
+    username = request.form.get("username")
+    password = request.form.get("password")
+    registry = request.form.get("registry")
+    try:
+        client.login(username, password=password, registry=registry)
+    except docker.errors.APIError:
+        abort(400, "Cannot login to registry")
 
-    client.login(username, password=password, registry=registry)
+    # push and wait
     rep = client.images.push(name)
     for r in rep.split('\n'):
-        if r and ast.literal_eval(r).get('error'):
-            return jsonify({'error': ast.literal_eval(r).get('error')})
+        if r and ast.literal_eval(r).get("error"):
+            abort(400, ast.literal_eval(r).get("error"))
     return Ok()
 
 
-def isSafePath(basedir, path):
-    return os.path.abspath(basedir + path).startswith(basedir)
-
-
-@app.route('/create', methods=['POST'])
+@app.route("/create", methods=["POST"])
 def create():
-    name = request.form.get('name')
+    """
+    Create the container
+
+    Parameters
+    ----------
+    name: str
+        The container name.
+    image: str
+        The base image you want to start.
+    homepath: str
+        The path you want to mount "homepath" to /home/ubuntu.
+        Default homepath = name
+    naspath: str
+        The path you want to mount "naspath" to /home/nas.
+        You can use more than one nas, use "," to split it.
+    command: str
+        Commands to execute.
+
+    Returns
+    ----------
+    Nothing will return.
+    You should check by "search".
+    """
+    # check container exist
+    name = request.form.get("name")
     if not name:
-        return Error('no name')
+        abort(400, "No Name")
     try:
         container = client.containers.get(name)
-        return Error('Exist')
+        abort(400, "Container Already Exist")
     except docker.errors.NotFound:
         pass
 
-    image = request.form.get('image')
+    # check image exist
+    image = request.form.get("image")
     try:
         client.images.get(image)
     except docker.errors.ImageNotFound:
-        return not_found('Not Find Image')
+        abort(400, "Image Not Found")
 
-    homepath = request.form.get('homepath')
-    if not homepath:
-        homepath = name
-    if not isSafePath(default_homepath, homepath):
-        return Error('Bad Path')
-
-    labnas = request.form.get('labnas', 'False').lower() == 'true'
+    # TODO: check path is valid
     mounts = []
-    if homepath:
-        mounts.append(docker.types.Mount('/home/ubuntu',
-                      default_homepath + homepath, type='bind'))
-    if labnas:
-        mounts.append(docker.types.Mount('/home/nas',
-                      default_naspath, type='bind'))
 
+    # Path to mount home dir
+    homepath = request.form.get("homepath")
+    if homepath:
+        mounts.append(docker.types.Mount("/home/ubuntu",
+                      homepath, type="bind"))
+
+    # Path to mount nas dir
+    naspath = request.form.get("naspath")
+    if naspath:
+        naspath = naspath.split(',')
+        if len(naspath) == 1:
+            mounts.append(docker.types.Mount("/home/nas",
+                          naspath[0], type="bind"))
+        else:
+            for p in naspath.split(','):
+                mounts.append(docker.types.Mount(
+                              "/home/nas/" + os.path.basename(p),
+                              p,
+                              type="bind"))
+
+    # create
     cont = client.containers.run(image,
                                  name=name,
                                  mounts=mounts,
-                                 command='tail -f /dev/null',  # test
-                                 labels={label: 'True'},
+                                 command=request.form.get("command"),
+                                 labels={label: "True"},
                                  detach=True,
-                                 restart_policy={'Name': 'always'})
+                                 restart_policy={'Name': "always"})
     return Ok()
 
 
-@app.route('/delete', methods=['POST'])
+@app.route("/delete", methods=["POST"])
 def delete():
-    name = request.form.get('name')
+    """Delete the container by name=ID"""
+    name = request.form.get("name")
     container = getContainer(name)
     container.stop()
     container.remove()
     return Ok()
 
 
-@app.route('/deleteImage', methods=['POST'])
+@app.route("/delete/image", methods=["POST"])
 def deleteImage():
-    name = request.form.get('name')
+    """Delete the image by name=ID"""
+    name = request.form.get("name")
     try:
         client.images.remove(name, force=True)
     except docker.errors.ImageNotFound:
-        return not_found('Not found Image')
+        abort(400, "Not found Image")
     return Ok()
 
 
-@app.route('/prune', methods=['POST'])
+@app.route("/prune", methods=["POST"])
 def prune():
+    """Clean staging images"""
     client.images.prune()
     return Ok()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3476)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3476)
